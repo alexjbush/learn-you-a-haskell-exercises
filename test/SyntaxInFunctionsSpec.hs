70a7faf83ee9f064bcd144dbd9ee706d61d80d34
Alex Bush <bushnoh@gmail.com>
2018-08-24 18:53:21 +0100

    Answers for syntax in functions

--- test/SyntaxInFunctionsSpec.hs
+++ test/SyntaxInFunctionsSpec.hs
@@ -1,23 +1,17 @@
 module SyntaxInFunctionsSpec where
 
-import           Test.Hspec
-import           SyntaxInFunctions
-import           Control.Exception (evaluate)
+import Control.Exception (evaluate)
+import SyntaxInFunctions
+import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "englishDigit" $ do
-    it "Print 3 as text" $ do
-      englishDigit 3 `shouldBe` "three"
-    it "favour the higher value when computing the average" $ do
-      englishDigit 10 `shouldBe` "unknown"
+    it "Print 3 as text" $ do englishDigit 3 `shouldBe` "three"
+    it "favour the higher value when computing the average" $ do englishDigit 10 `shouldBe` "unknown"
   describe "divTuple" $ do
-  	it "divide 4 by 2" $ do
-  	  divTuple (4, 2) `shouldBe` 2
-  	it "divide by zero is undefined" $ do
-  	  evaluate (divTuple (4, 0)) `shouldThrow` anyArithException
+    it "divide 4 by 2" $ do divTuple (4, 2) `shouldBe` 2
+    it "divide by zero is undefined" $ do evaluate (divTuple (4, 0)) `shouldThrow` errorCall "Prelude.undefined"
   describe "threeZeroList" $ do
-  	it "return true if passed a list starting with three zeros" $ do
-  	  threeZeroList [0, 0, 0, 2] `shouldBe` True
-  	it "return false if passed a list not starting with three zeros" $ do
-  	  threeZeroList [] `shouldBe` False
+    it "return true if passed a list starting with three zeros" $ do threeZeroList [0, 0, 0, 2] `shouldBe` True
+    it "return false if passed a list not starting with three zeros" $ do threeZeroList [] `shouldBe` False
