73f964b94724ed5f1f264a17f08d685830b18376
Alex Bush <bushnoh@gmail.com>
2018-09-23 21:31:02 +0100

    Modules module and some of Own Types

--- src/Modules.hs
+++ src/Modules.hs
@@ -1,9 +1,14 @@
+module Modules
+  ( areaConv
+  , sqInToSqCm
+  , sqChainsToSqM
+  ) where
+
 -- Fill in the DistanceConversions module first, and import it here
 -- create a higher-order function for converting an area between two dimensions
 -- this will take the function for converting a distance, and an area to convert
 -- using the functions defined in the DistanceConversions module
 -- Example areaConv inchesToCentimetres 9 = 58.0644
-
 areaConv :: (Float -> Float) -> Float -> Float
 areaConv linearConversion area = undefined
 
---
+++ test/MakingOurOwnTypesAndTypeclassesSpec.hs
@@ -0,0 +1,9 @@
+module MakingOurOwnTypesAndTypeclassesSpec where
+
+import           MakingOurOwnTypesAndTypeclasses
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "betterCard" $ it "???" $ "???" `shouldBe` "???"
+  describe "betterCard2" $ it "???" $ "???" `shouldBe` "???"
